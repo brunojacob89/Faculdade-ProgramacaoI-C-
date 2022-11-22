70c5d3c3edf4670acf737d4bb0f7a4a2ca3fe2e3
brunojacob <brunojacob_89@hotmail.com>
2022-11-22 00:03:17 -0300

    recursividade e tratamento

---
+++ Matrizes/Program.cs
@@ -0,0 +1,29 @@
+﻿
+int[,] numeros = new int[4,4];
+
+numeros[0,0] = 10;
+numeros[0,1] = 20;
+numeros[0,2] = 30;
+numeros[0,3] = 40;
+numeros[1,0] = 12;
+numeros[1,1] = 43;
+numeros[1,2] = 33;
+numeros[1,3] = 54;
+numeros[2,0] = 6;
+numeros[2,1] = 65;
+numeros[2,2] = 34;
+numeros[2,3] = 6;
+numeros[3,0] = 34;
+numeros[3,1] = 67;
+numeros[3,2] = 56;
+numeros[3,3] = 75;
+
+for(int i = 0; i < 4; i++){
+    for(int j = 0; j < 4; j++){
+        Console.Write(numeros[i,j]+ "  ");
+    }
+    Console.WriteLine();
+    
+}
+
+
---
+++ Recursividade-tratamento-de-exceções-e-persistência-de-dados/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Recursividade_tratamento_de_exceções_e_persistência_de_dados;
+class Program
+{
+    static void funcaoRecursiva(int numero)
+    {
+        if(numero < 4)
+        {
+            funcaoRecursiva(numero + 1);
+        }
+        Console.WriteLine(numero);
+    }
+    static void Main(string[] args)
+    {
+       // funcaoRecursiva(0);
+
+       Console.Write("Digite um número par saber sua raiz quadrada: ");
+       try
+       {
+            double numero = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"A raiz quadrada de {numero} é {Math.Sqrt(numero)}");
+       }
+       catch(FormatException)
+       {
+        Console.WriteLine("Número invalido!");
+       }
+       
+      
+    }
+}
